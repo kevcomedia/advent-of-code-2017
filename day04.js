dcc2c6cf673857e1e2f3c9f01447588b0f0dc180
Kev Comedia <kevcomedia@gmail.com>
2017-12-04 13:50:03 +0800

    Solve day 4 part 2

--- day04.js
+++ day04.js
@@ -10,4 +10,16 @@ function isValid(passphrase) {
   return words.length == [...new Set(words)].length;
 }
 
-console.log(input.filter(isValid).length);
+assert.equal(isValidNoAnagrams('abcde fghij'), true);
+assert.equal(isValidNoAnagrams('abcde xyz ecdab'), false);
+assert.equal(isValidNoAnagrams('a ab abc abd abf abj'), true);
+assert.equal(isValidNoAnagrams('iiii oiii ooii oooi oooo'), true);
+assert.equal(isValidNoAnagrams('oiii ioii iioi iiio'), false);
+
+function isValidNoAnagrams(passphrase) {
+  const words = passphrase.split(' ');
+  const sorted = words.map((word) => [...word].sort().join(''));
+  return sorted.length == [...new Set(sorted)].length;
+}
+
+console.log(input.filter(isValidNoAnagrams).length);
