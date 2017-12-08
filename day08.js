502a41514341349f521c94477d26f0ce6e652d57
Kev Comedia <kevcomedia@gmail.com>
2017-12-08 13:48:50 +0800

    Refactor day 8

--- day08.js
+++ day08.js
@@ -4,51 +4,47 @@ a inc 1 if b < 5
 c dec -10 if a >= 1
 c inc -20 if c == 10`;
 
-const ops = {
-  '==': (x, y) => x == y,
-  '!=': (x, y) => x != y,
-  '<':  (x, y) => x < y,
-  '<=': (x, y) => x <= y,
-  '>':  (x, y) => x > y,
-  '>=': (x, y) => x >= y,
-};
+function createOpFn(op, val) {
+  switch (op) {
+    case '==': return (x) => x == val;
+    case '!=': return (x) => x != val;
+    case '<': return (x) => x < val;
+    case '<=': return (x) => x <= val;
+    case '>': return (x) => x > val;
+    case '>=': return (x) => x >= val;
+  };
+}
 
 function produceRegisters(instructionString) {
   return instructionString.split('\n')
-    .reduce((registers, line) => {
-      const registerName = line.split(' ')[0];
-      registers[registerName] = 0;
-    }, {});
-  const registers = {};
-  for (let ins of instructions.split('\n')) {
-    const name = ins.split(' ')[0];
-    registers[name] = 0;
-  }
-  return registers;
+    .reduce((registers, line) =>
+      (registers[line.match(/^\w+/)] = 0, registers), {});
 }
 
-function parseInstructions(instructions) {
-  return instructions.split('\n')
+function parseInstructionString(instructionString) {
+  return instructionString.split('\n')
     .map((line) => {
-      const tokens = line.split(' ');
-      const name = tokens[0];
-      const delta = tokens[1] == 'inc' ? +tokens[2] : -(+tokens[2]);
-      const predTokens = line.split(' if ')[1].split(' ');
-      const predicate = {
-        reg: predTokens[0],
-        op: ops[predTokens[1]],
-        val: +predTokens[2],
+      const [name, mode, modeVal] = line.split(' ');
+      const [predReg, op, predVal] = line.split(' if ')[1].split(' ');
+
+      return {
+        name,
+        delta: mode == 'inc' ? +modeVal : -(+modeVal),
+        predicate: {
+          reg: predReg,
+          op: createOpFn(op, +predVal)
+        }
       };
-      return {name, delta, predicate};
     });
 }
 
-function execKeepMax(instructions, register) {
+function execKeepMax(instructions, registers) {
   let max = Number.MIN_SAFE_INTEGER;
+
   instructions.forEach((instruction) => {
-    if (instruction.predicate.op(register[instruction.predicate.reg], instruction.predicate.val)) {
-      register[instruction.name] += instruction.delta;
-      max = Math.max(max, register[instruction.name]);
+    if (instruction.predicate.op(registers[instruction.predicate.reg])) {
+      registers[instruction.name] += instruction.delta;
+      max = Math.max(max, registers[instruction.name]);
     }
   });
 
@@ -62,8 +58,8 @@ function maxValue(registers) {
 const instructionString = input;
 
 const registers = produceRegisters(instructionString);
-const instructions = parseInstructions(instructionString);
+const instructions = parseInstructionString(instructionString);
 
 const max = execKeepMax(instructions, registers);
 
-console.log(max);
+console.log({max, registers});
