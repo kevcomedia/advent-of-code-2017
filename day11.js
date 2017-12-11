c19790da16f0ba36ba9a6dc547fdd353b25d3f77
Kev Comedia <kevcomedia@gmail.com>
2017-12-11 16:15:21 +0800

    Solve day 11

---
+++ day11.js
@@ -0,0 +1,57 @@
+const input = require('./day11_input');
+
+const max = (a, b) => Math.max(a, b);
+
+const x = input;
+const parsed = x.split(',');
+
+function countSteps(parsed) {
+  const path = parsed.slice(0);
+  let ns = 0;
+  let nesw = 0;
+  let nwse = 0;
+
+  let maxDistance = 0;
+  for (let i = 0; i < path.length; i++) {
+    switch (path[i]) {
+      case 'n':
+        ns++;
+        break;
+      case 's':
+        ns--;
+        break;
+      case 'ne':
+        nesw++;
+        break;
+      case 'nw':
+        nwse++;
+        break;
+      case 'se':
+        nwse--;
+        break;
+      case 'sw':
+        nesw--;
+        break;
+    }
+
+    let maxEdge;
+    let center;
+
+    if ((nesw >= 0 && nwse >= 0) || (nesw < 0 && nwse < 0)) {
+      maxEdge = [nesw, nwse].map(Math.abs).reduce(max);
+      center = Math.abs(ns);
+    } else if ((ns >= 0 && nwse < 0) || (ns < 0 && nwse >= 0)) {
+      maxEdge = [ns, nwse].map(Math.abs).reduce(max);
+      center = Math.abs(nesw);
+    } else {
+      maxEdge = [ns, nesw].map(Math.abs).reduce(max);
+      center = Math.abs(nwse);
+    }
+
+    maxDistance = Math.max(maxDistance, center + maxEdge);
+  }
+
+  return maxDistance;
+}
+
+console.log(countSteps(parsed));
