b2b04dd74afe81a6c48e2623ead943350cbb6009
Kev Comedia <kevcomedia@gmail.com>
2017-12-25 11:42:57 +0800

    Solve day 24 part 1

---
+++ day24.js
@@ -0,0 +1,52 @@
+const input = require('fs')
+  .readFileSync('./day24_input', 'utf8')
+  .trim();
+
+const test = `0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10`;
+
+const parts = input.split('\n').map(p => ({
+  pins: new Set(p.split('/').map(Number)),
+  used: false,
+}));
+
+function computePartStrength(part) {
+  const pins = [...part];
+  return pins.length == 2 ? pins[0] + pins[1] : 2 * pins[0];
+}
+
+function getOtherPin(part, pin) {
+  if (part.pins.size == 1) {
+    return pin;
+  }
+
+  const pins = [...part.pins];
+  return pin == pins[0] ? pins[1] : pins[0];
+}
+
+function calculateStrongest(parts, start = 0, total = 0) {
+  const unusedParts = parts.filter(p => !p.used).filter(p => p.pins.has(start));
+  if (unusedParts.length == 0) {
+    return total;
+  }
+
+  let max = 0;
+
+  for (let i = 0; i < unusedParts.length; i++) {
+    const part = unusedParts[i];
+    const end = getOtherPin(part, start);
+    part.used = true;
+    max = Math.max(max, calculateStrongest(parts, end, total + start + end));
+    part.used = false;
+  }
+
+  return max;
+}
+
+console.log(calculateStrongest(parts));
